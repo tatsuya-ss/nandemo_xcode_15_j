9bc009454a0e21eece0833efee27e711ec81d8ac
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2024-05-21 10:43:37 +0900

    Initial Commit

---
+++ nandemo_xcode_15_j/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nandemo_xcode_15_j
+//
+//  Created by tatsuya.sakamoto on 2024/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
